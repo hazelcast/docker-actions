a5be433e64ac0da03f07c642512bf18b969f4a3d
Jack Green <JackPGreen@Gmail.com>
2025-11-17 10:18:22 +0000

    Fix RedHat publish
    See https://github.com/hazelcast/hazelcast-docker/pull/1163

--- resolve-editions/build.functions.sh
+++ resolve-editions/build.functions.sh
@@ -1,14 +1,14 @@
 set -euo pipefail ${RUNNER_DEBUG:+-x}
 
 # Checks if we should build the OSS artefacts.
-# Returns "yes" if we should build it or "no" if we shouldn't.
+# Returns "true" if we should build it or "false" if we shouldn't.
 function should_build_oss() {
 
   local release_type=$1
   if [[ $release_type =~ ^(ALL|OSS)$ ]]; then
-    echo "yes"
+    echo "true"
   else
-    echo "no"
+    echo "false"
   fi
 }
 
@@ -17,8 +17,8 @@ function should_build_ee() {
 
   local release_type=$1
   if [[ $release_type =~ ^(ALL|EE)$ ]]; then
-    echo "yes"
+    echo "true"
   else
-    echo "no"
+    echo "false"
   fi
 }
